a73eced017f8af44cd42058a790d595912f83ebe
Ratnesh Singh <rsingh@rsingh-pk.local>
2020-04-26 09:15:43 -0700

    Added Starter Project

---
+++ SwiftUISample/SwiftUISample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISample
+//
+//  Created by rsingh on 4/26/20.
+//  Copyright © 2020 Ratnesh Dev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
